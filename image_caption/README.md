6570a051d7d406df3321b34073798413db6dc27b
bovey0809 <my364007886@gmail.com>
2019-12-23 20:24:49 +0900

    initialize rnn sentiment analyse

---
+++ image_caption/README.md
@@ -0,0 +1,3 @@
+$$e_{i j}=v_{a}^{\top} \tanh \left(W_{a} s_{i-1}+U_{a} h_{j})a_{i j}=\frac{\exp \left(e_{i j})}{\sum_{k=1}^{T_{x}} \exp \left(e_{i k}} \\ c_{i}=& \sum_{j=1}^{T_{x}} \alpha_{i j} h_{j}$$
+
+$$\alpha$$
--- image_caption/playground.py
+++ image_caption/playground.py
@@ -1,4 +1,11 @@
-#%%
-\(e_{i j}=v_{a} ^ {\top} \tanh \left(W_{a} s_{i-1}+U_{a} h_{j})
-  a_{i j}=\frac{\exp \left(e_{i j})}
-  {\sum_{k=1} ^ {T_{x}} \exp \left(e_{i k}} \\ c_{i}= & \sum_{j=1} ^ {T_{x}} \alpha_{i j} h_{j} \)
+#%% [markdown]
+# \(e_{i j}=v_{a} ^ {\top} \tanh \left(W_{a} s_{i-1}+U_{a} h_{j})a_{i j}=\frac{\exp \left(e_{i j})}{\sum_{k=1} ^ {T_{x}} \exp \left(e_{i k}} \\ c_{i}= & \sum_{j=1} ^ {T_{x}} \alpha_{i j} h_{j} \)
+
+
+# %%
+# %%
+df = ts.get_h_data('600848')
+
+# %%
+import torch
+print(torch._nnpack_available())
